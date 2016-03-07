cea79557bd2a79684d0cd49fa5ef24c6ad76562e
ddalton002 <ddalton002@student.butte.edu>
2016-03-07 23:50:02 +0000

    test

---
+++ Topic 2/lab 5/lab_5.cpp
@@ -0,0 +1,18 @@
+/*
+Define a "header only" template class name Box with:
+- one data member -- contents (data type template)
+- overloaded constructor -- one parameter newContents to be assigned to contents
+- getter (getContents) and setter (setContents)
+- friend overloaded operator <<
+*/
+
+/*
+Create a template function
+
+ * Function Name: Sum
+ * Return the "sum" of the values in the array. Your initial sum 	should be set
+ * to "zero" in your algorithm. To do this use T().
+ * @param T values - An array of any type
+ * @param unsigned int size - The size of the array
+ * @return T - The sum of the values in the array
+ */
