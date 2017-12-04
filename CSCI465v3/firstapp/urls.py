b3098ef702b4aa3e07f758d6aa3f70ed72e66f88
ddalton002 <ddalton3@mail.csuchico.edu>
2017-12-03 19:38:36 -0800

    Add files via upload

---
+++ CSCI465v3/firstapp/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$',views.index, name='index'),
+    url(r'page2$',views.page2, name='page2'),
+    url(r'register/$',views.register, name='register'),
+    url(r'suggestions/$',views.suggestions, name='suggestions'),
+    url(r'^suggest/$',views.suggestion_view, name='suggest'),
+    url(r'profile/$',views.profile, name='profile'),
+    url(r'lfg/$',views.lfg, name='lfg'),
+    url(r'lfm/$',views.lfm, name='lfm'),
+    url(r'wtb/$',views.wtb, name='wtb'),
+    url(r'wts/$',views.wts, name='wts'),
+
+]
